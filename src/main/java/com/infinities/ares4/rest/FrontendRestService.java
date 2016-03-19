6f9757c6b4b31eb2cb27b04165bc9500d4df1863
Nat <nat@infinitiessoft.com>
2016-03-19 10:51:52 +0800

    added rest and html folder

---
+++ src/main/java/com/infinities/ares4/rest/FrontendRestService.java
@@ -0,0 +1,11 @@
+package com.infinities.ares4.rest;
+
+import org.jboss.logging.Logger;
+
+public class FrontendRestService {
+	
+	private Logger LOG = Logger.getLogger(FrontendRestService.class);
+	
+	
+
+}
